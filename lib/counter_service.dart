96e6d9d73a52b167f5cbdbf72ddd9e61a694c17d
Ahmed Naeem <ahmednaeem1012@gmail.com>
2023-12-28 13:23:34 +0500

    Grouping Notifications and Adding Extension for viewing

--- lib/counter_service.dart
+++ lib/counter_service.dart
@@ -1,4 +1,6 @@
-// ignore_for_file: deprecated_member_use
+// ignore_for_file: deprecated_member_use, unused_element
+
+import 'dart:math';
 
 import 'package:flutter/foundation.dart';
 
@@ -29,46 +31,62 @@ class CounterService {
 
     await _flutterLocalNotificationsPlugin.initialize(
       initSettings,
-      onSelectNotification: _onSelectNotification,
+      // onSelectNotification: _onSelectNotification,
     );
   }
 
   final List<String> _notificationMessages = [
-    'لا تشارك كود الواتسب مع أي شخص', // 'Do not share your WhatsApp code with anyone'
-    'لا تفتح رابط مشبوه', // 'Do not open a suspicious link'
-    'أجعل الارقام السرية قوية' // 'Make your passwords strong'
+    // 'This is a long test notification message to check if the expandable feature works correctly when the notification is pulled down by the user.'
+
+    "تأكد من مصداقية المصادر التي تطلب منك معلومات حساسة.",
+    "قم بتحديث كلمات المرور الخاصة بك بشكل دوري",
+    "لا تشارك أبدًا معلومات حساب البنك عبر الهاتف أو البريد الإلكتروني",
+    "تأكد من إغلاق جميع الجلسات الخاصة بك عند استخدام أجهزة عامة",
+    "استخدام كلمات مرور قوية: اختر كلمات مرور قوية وفريدة لكل حساب، وتجنب استخدام نفس كلمة المرور لأكثر من حساب",
+    "تغيير كلمات المرور بانتظام: قم بتحديث كلمات المرور بشكل دوري لزيادة مستوى الأمان.",
+    "التحقق الثنائي للهوية: فعّل خاصية التحقق الثنائي لحساباتك الحساسة لإضافة طبقة إضافية من الحماية.",
+    "تفعيل إعدادات الأمان في الأجهزة: استخدم إعدادات الأمان المتاحة في أجهزتك مثل قفل الشاشة وميزات الحماية الأخرى.",
+    "تحديد الأذونات للتطبيقات: قم بمراجعة وتحديد الأذونات التي تمنحها لتطبيقاتك لتقليل المخاطر الأمانية.",
+    "التبليغ عن الاختراقات: في حال الشك في اختراق أو نشاط غير مألوف، قم بالإبلاغ فورًا إلى الجهات المعنية.",
+    "التوعية الرقمية: قم بتوعية نفسك والآخرين حول مخاطر الأمان الرقمي وكيفية تجنبها.",
+    "الابتعاد عن الشكل العاملي: تجنب مشاركة معلومات حساسة عبر الهاتف أو البريد الإلكتروني إلا إذا كنت واثقًا من المصدر.",
+    "التحقق من فواتير البنك: راجع فواتير البنك بانتظام للكشف عن أي نشاط غير مألوف.",
   ];
+
   void _onSelectNotification(String? payload) {}
 
   void startCounting() {
-    Stream.periodic(Duration(seconds: 10)).listen((_) async {
+    Stream.periodic(Duration(seconds: 5)).listen((_) async {
       _counter.increment();
       await _scheduleNotification(_counter.count.value);
     });
   }
 
   Future<void> _scheduleNotification(int count) async {
-    // Step 2: Modify the method to select a message based on the count
-    var scheduledNotificationDateTime =
-        DateTime.now().add(const Duration(seconds: 10));
     var androidDetails = const AndroidNotificationDetails(
-      'channel_id',
-      'channel_name',
-      importance: Importance.max,
-      priority: Priority.high,
-    );
+        'channel_id', 'channel_name',
+        importance: Importance.max,
+        priority: Priority.high,
+        styleInformation: BigTextStyleInformation(''),
+        indeterminate: true,
+        enableLights: true,
+        fullScreenIntent: true,
+        setAsGroupSummary: true,
+        autoCancel: false,
+        showWhen: true,
+        ongoing: true);
     var platformDetails = NotificationDetails(android: androidDetails);
 
-    // Step 3: Use modulo operator to loop through the notifications
     String notificationMessage =
         _notificationMessages[count % _notificationMessages.length];
-
-    await _flutterLocalNotificationsPlugin.schedule(
-      0,
-      'Abtzaz',
-      '$notificationMessage',
-      scheduledNotificationDateTime,
-      platformDetails,
+    int notificationId = Random().nextInt(1000000);
+    // Show the notification immediately
+    await _flutterLocalNotificationsPlugin.show(
+      notificationId, // Unique ID for the notification
+      'Abtzaz', // Title
+      notificationMessage, // Body
+      platformDetails, // Notification details
+      payload: 'Item $count', // Optional payload
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,18 @@
+import 'dart:developer';
 import 'dart:ui';
 
 import 'package:abtzaz/app_retain_widget.dart';
 import 'package:abtzaz/background_main.dart';
 import 'package:abtzaz/counter_service.dart';
+import 'package:abtzaz/services/notification_services.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+
 import 'package:flutter/services.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MyApp());
 
   var channel = const MethodChannel('com.example/background_service');
@@ -35,6 +41,18 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  NotificationServices notificationServices = NotificationServices();
+  @override
+  void initState() {
+    notificationServices.requestNotificationPermission();
+    notificationServices.firebaseInit(context);
+    notificationServices.isTokenRefresh();
+
+    notificationServices.getDeviceToken().then((value) => print(value));
+    super.initState();
+  }
+
+  bool check = false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -50,40 +68,72 @@ class _MyHomePageState extends State<MyHomePage> {
         ),
         backgroundColor: Colors.black,
       ),
-      body: Center(
-        child: ValueListenableBuilder(
-          valueListenable: CounterService.instance().count,
-          builder: (context, count, child) {
-            return Padding(
-              padding: EdgeInsets.only(top: 30, right: 20, left: 25),
-              child: SizedBox(
-                width: double
-                    .infinity, // to make the button take the full available width
-                child: ElevatedButton(
-                  onPressed: () {},
-                  style: ButtonStyle(
-                    backgroundColor: MaterialStateProperty.all(
-                        Color.fromARGB(255, 71, 114, 145)),
-                    padding: MaterialStateProperty.all(
-                        const EdgeInsets.symmetric(
-                            horizontal: 13, vertical: 15)),
-                    shape: MaterialStateProperty.all(
-                      RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(8)),
-                    ),
-                  ),
-                  child: Text(
-                    ('Let Me In'),
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 16,
-                    ),
+      body: Column(
+        mainAxisAlignment:
+            MainAxisAlignment.spaceBetween, // Adjusts main axis alignment
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Padding(
+            padding: EdgeInsets.only(right: 15, left: 15, top: 30),
+            child: Text(
+              'تطبيق الابتزاز يقدم نصائح فورية لتوعية المستخدمين حول مخاطر الابتزاز الإلكتروني، ضمانًا لسلامتهم الرقمية',
+              textAlign: TextAlign.right,
+              style: TextStyle(
+                  fontSize: 17,
+                  fontWeight: FontWeight.w600,
+                  color: Color.fromARGB(255, 14, 12, 64)),
+            ),
+          ),
+          Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(top: 0.0),
+                child: Center(
+                  child: ValueListenableBuilder(
+                    valueListenable: CounterService.instance().count,
+                    builder: (context, count, child) {
+                      return Padding(
+                        padding: EdgeInsets.only(top: 0, right: 20, left: 25),
+                        child: GestureDetector(
+                          onTap: () {
+                            setState(() {
+                              check = true;
+                              log(check.toString());
+                            });
+                          },
+                          child: SizedBox(
+                              child: Image.asset(
+                            check == false
+                                ? 'assets/images/1.png'
+                                : 'assets/images/2.png',
+                            height: 200,
+                            width: 200,
+                          )),
+                        ),
+                      );
+                    },
                   ),
                 ),
               ),
-            );
-          },
-        ),
+              Padding(
+                padding: EdgeInsets.only(right: 20, left: 25, bottom: 35),
+                child: Text(
+                  '"نحن مستعدون لاستماع اقتراحاتكم ومشاركتكم في تطوير التطبيق. يُرجى التواصل معنا عبر الأرقام التالية لتبادل أفكاركم وتجاربكم. شكرًا لتعاونكم في جعل التطبيق أفضل!"',
+                  textAlign: TextAlign.right,
+                  style: TextStyle(),
+                ),
+              ),
+            ],
+          ),
+          Padding(
+            padding: const EdgeInsets.only(bottom: 20.0),
+            child: Text(
+              '0317043169',
+              style: TextStyle(
+                  fontSize: 14, color: Color.fromARGB(250, 60, 60, 60)),
+            ),
+          )
+        ],
       ),
     );
   }
